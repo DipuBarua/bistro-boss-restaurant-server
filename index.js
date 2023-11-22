fc4858b4326221c3fbc28a3959825564eea07df4
DipuBarua <you@example.com>
2023-11-23 05:34:29 +0600

    created jwt for securing admin role, verify token,user collection related API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -32,7 +33,51 @@ async function run() {
         const cartCollection = client.db("bistroDB").collection("carts");
         const userCollection = client.db("bistroDB").collection("users");
 
+        // jwt - API 
+        app.post("/jwt", async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
+        })
+
+        // middleware - jwt verification
+        const verifyToken = (req, res, next) => {
+            console.log("inside verify token", req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: "forbidden access" })
+            }
+            const token = req.headers.authorization.split(' ')[1]
+
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: "forbidden access" });
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
         // user collection - API 
+        app.get("/users", verifyToken, async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.get("/users/admin/:email", verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: "unauthorized access" });
+            }
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+
+            let admin = false;
+            if (user) {
+                admin = (user?.role === "admin");
+            }
+            res.send({ admin });
+        })
+
         app.post("/users", async (req, res) => {
             const user = req.body;
 
@@ -44,6 +89,25 @@ async function run() {
             }
 
             const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.patch("/users/admin/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateUser = {
+                $set: {
+                    role: "admin"
+                }
+            }
+            const result = await userCollection.updateOne(filter, updateUser);
+            res.send(result);
+        })
+
+        app.delete("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
             res.send(result);
         })
 
