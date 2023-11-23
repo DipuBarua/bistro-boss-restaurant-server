8d0532b87b1480907618938b52ebdd7aeba5c03e
DipuBarua <you@example.com>
2023-11-24 04:29:33 +0600

    updated admin verify middelware, and used,post for menu collection also

--- index.js
+++ index.js
@@ -44,30 +44,44 @@ async function run() {
         const verifyToken = (req, res, next) => {
             console.log("inside verify token", req.headers.authorization);
             if (!req.headers.authorization) {
-                return res.status(401).send({ message: "forbidden access" })
+                return res.status(401).send({ message: "unauthorized access" })
             }
             const token = req.headers.authorization.split(' ')[1]
 
             jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
                 if (err) {
-                    return res.status(401).send({ message: "forbidden access" });
+                    return res.status(401).send({ message: "unauthorized access" });
                 }
                 req.decoded = decoded;
                 next();
             })
         }
 
+        // middelware - use verify admin after getting verifyToken
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = (user?.role === "admin");
+            if (!isAdmin) {
+                return res.status(403).send({ message: "forbidden access" })
+            }
+            next();
+        }
+
         // user collection - API 
-        app.get("/users", verifyToken, async (req, res) => {
+        app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
         })
 
         app.get("/users/admin/:email", verifyToken, async (req, res) => {
             const email = req.params.email;
+
             if (email !== req.decoded.email) {
-                return res.status(403).send({ message: "unauthorized access" });
+                return res.status(403).send({ message: "forbidden access" });
             }
+
             const query = { email: email };
             const user = await userCollection.findOne(query);
 
@@ -92,7 +106,7 @@ async function run() {
             res.send(result);
         })
 
-        app.patch("/users/admin/:id", async (req, res) => {
+        app.patch("/users/admin/:id", verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const updateUser = {
@@ -104,26 +118,31 @@ async function run() {
             res.send(result);
         })
 
-        app.delete("/users/:id", async (req, res) => {
+        app.delete("/users/:id", verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await userCollection.deleteOne(query);
             res.send(result);
         })
 
-        // menu collection 
+        // menu collection - API
         app.get("/menu", async (req, res) => {
             const result = await menuCollection.find().toArray();
             res.send(result);
         })
+        app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
+            const menuItem = req.body;
+            const result = await menuCollection.insertOne(menuItem);
+            res.send(result);
+        })
 
-        // review collection 
+        // review collection - API
         app.get("/reviews", async (req, res) => {
             const result = await reviewCollection.find().toArray();
             res.send(result);
         })
 
-        // cart collection 
+        // cart collection - API
         app.get('/carts', async (req, res) => {
             const email = req.query.email;
             const query = { email: email }
